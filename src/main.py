1f9b1205104d7b14f6f807056bfdb261709e79e3
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-12-28 14:41:25 +0100

    Enh:  Entrypoint build process call

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import argparse
 import logging
 
-from builder import ModpackBuilder
+from builder import ModpackBuilder, ModpackBuilderOptions
 
+# Helper function used to define all supported CLI flags and --help docs
 def create_argument_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     
@@ -12,14 +13,31 @@ def create_argument_parser() -> argparse.ArgumentParser:
         required=True,
         help='The path to the mod pack configuration JSON file.')
 
+    # TODO: We can use the latest version when this flag is not specified
+    parser.add_argument('-b', '--build-version',
+        action='store',
+        dest='buildVersion',
+        required=True,
+        help='The target modding API version.')
+
+    # TODO: We can use a flag to specify if we want to build to a directory or .zip file
+    # TODO: We can use a flag to specify how to handle unavailable resources (crashing the build or skiping a given resource) 
+
     parser.add_argument('-v', '--verbose',
         action='store_true',
         dest='verboseMode',
         required=False,
         help='Log more details about the modpack building process.')
 
+    parser.add_argument('-s', '--skip-checksum',
+        action='store_true',
+        dest='skipChecksum',
+        required=False,
+        help='Skip the process of checking the mod file hash.')
+
     return parser
 
+# Helper function used to configure the behavior of the logging system
 def configure_logging(verboseMode: bool) -> None:
     logLevel = logging.INFO
     if verboseMode: logLevel = logging.DEBUG
@@ -38,7 +56,17 @@ if __name__ == "__main__":
     
     configure_logging(args.verboseMode)
     
-    # TODO: Currently the builder is case-sensitive (Json props), this is the price of choosing Python...
-    builder = ModpackBuilder(args.modpackFilePath)
-
-    exit(0)
+    try:
+        # TODO: Currently the builder is case-sensitive (Json props), this is the price of choosing Python...
+        builderOptions = ModpackBuilderOptions(skipChecksum=args.skipChecksum)
+        builder = ModpackBuilder(args.modpackFilePath, builderOptions)
+        builder.build()
+        
+        exit(0)
+    except Exception as ex:
+        if hasattr(ex, 'message'):
+            logging.error(ex.message)
+        else:
+            logging.error(ex)
+        
+        exit(1)
